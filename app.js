a002cd2253e3414b5710ad5b9772cdcc13cbea4c
mervynlin <linhuiman@gmail.com>
2016-09-27 02:07:11 +0800

    first comment

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-//add comment
+//add commentb1
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
